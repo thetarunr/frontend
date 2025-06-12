11dfca079ed890a406a7feafb2b9a313981cf113
Rajasekar K <rajasekark@Rajasekars-MacBook-Pro.local>
2025-06-12 13:57:05 +0530

    header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Activity } from 'lucide-react';
-
+import logo from "../../dist/assets/LEVEL UP TURF-1.png";
 const Header: React.FC = () => {
   return (
-    <header className="bg-white shadow-sm">
+    <header className="bg-black shadow-sm">
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
-            <Activity className="h-8 w-8 text-green-600" />
-            <div className="font-bold text-xl md:text-2xl text-gray-800">
+          <img
+  src={logo}
+  alt="Level Up Turf Logo"
+  className="h-14 w-auto border-none"
+/>
+
+            <div className="font-bold text-xl md:text-2xl text-white">
               Level <span className="text-green-600">Up</span>
             </div>
           </div>
@@ -16,7 +20,7 @@ const Header: React.FC = () => {
           <nav className="hidden md:flex items-center space-x-6">
             
            
-            <a href="/gallery" className="text-gray-700 hover:text-green-600 transition-colors font-medium">
+            <a href="/gallery" className="text-white hover:text-green-600 transition-colors font-medium">
               Gallery
             </a>
            
